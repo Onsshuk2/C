7b7c7872676ea225e5a313413500717a1c45ffa1
onssh <onssh@DESKTOP-LCTPOKA>
2024-02-28 19:20:40 +0000

    Add project files.

---
+++ Intro1.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{//EX1
+	cout << "\"Terrifying words when they are silent,\n";
+	cout << "When they suddenly lurked,\n";
+	cout << "When you don't know where to start them,\n";
+	cout << "Because all words were already someone's\"\n";
+	cout << "\t\t\t\t  Lina Kostenko\n";
+ //EX2
+	cout << "\"Richard Of York Gave Battle In Vain\n";
+	cout << "Red\n";
+	cout << "Yellow\n";
+	cout << "Green\n";
+	cout << "Blue\n";
+	cout << "Inigo\n";
+	cout << "Violet\n";
+ //EX3
+	cout << "Example\n";
+	cout << "Puppies for sale.\n";
+	cout << "Rottweiler breed\n";
+	cout << "...\n";
+	cout << "Phone: 222-22-22\n";
+	cout << "222\t|222\t|\n";
+	cout << "22\t|22\t|\n";
+	cout << "22\t|22\t|\n\n";
+
+
+
+}
