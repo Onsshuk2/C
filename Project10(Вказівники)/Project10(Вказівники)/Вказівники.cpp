c9695610602f93394bb0280f488753126af9c0d8
Ira <152921559+Onsshuk2@users.noreply.github.com>
2024-03-19 19:59:04 +0000

    Add files via upload

---
+++ Project10(Вказівники)/Project10(Вказівники)/Вказівники.cpp
@@ -0,0 +1,74 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	//Ex1
+	int arr1[5]{1,2,3,4,5 };
+	int arr2[10]{0};
+	
+	int *parr1=arr1;
+	int *parr2=arr2;
+
+	for (size_t i = 0; i < 5; i++)
+	{
+		*(parr2 + i) = *(parr1 + i);
+	}
+	cout << "First: ";
+	for (int i = 0; i < 5; i++) {
+		cout << arr1[i] << " ";
+	}cout << endl;
+
+	cout << "Second: ";
+	for (int i = 0; i < 5; i++) {
+		cout << arr2[i] << " ";
+	}cout << endl;
+
+	//Ex2
+    int const SIZE = 5;
+    int myArray[]{ 1, 2, 3, 4, 5 }; 
+    cout << "First: ";
+    for (int i = 0; i < SIZE; i++) {
+        cout << myArray[i] << " ";
+    }
+    int* start = myArray;
+    int* end = myArray + SIZE - 1;
+    while (start < end) {
+
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+
+        start++;
+        end--;
+    }cout << endl;
+
+    cout << "New: ";
+    for (int i = 0; i < SIZE; i++) {
+        cout << myArray[i] << " ";
+    }cout << endl;
+
+	//Ex3
+    int arr01[] = { 1, 2, 3, 4, 5 };
+    int arr02[5]; 
+
+    int* ptr1 = arr01;
+    int* ptr2 = arr02 + 4; 
+
+    for (int i = 0; i < 5; i++) {
+        *(ptr2 - i) = *(ptr1 + i);
+    }
+
+    
+    cout << "First: ";
+    for (int i = 0; i < 5; i++) {
+        cout << arr01[i] << " ";
+    }cout << endl;
+
+    cout << "Second: ";
+    for (int i = 0; i < 5; i++) {
+        cout << arr02[i] << " ";
+    }
+
+}
